d24515758b78964f138d47edef21bba3a809d729
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-20 17:29:10 +0800

    Update test.py

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,9 @@
-name = None
-age = None
-isMale = None
-department = None
+name = "Chezter Lei Christian Reblando"
+department= "Department of Software and Web Development"
+about_me = "I love coding"
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+
+
+
+print(f"Hello, I am {name}. I love {department} and I love in backend even more!")
+print(f"One thing about me is that {about_me}.")
