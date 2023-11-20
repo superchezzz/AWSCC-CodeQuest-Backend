b1ce1c6288ef9f123aa3add8e7f892063a6bd7a8
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-20 23:30:11 +0800

    Create mini-challenge.py

---
+++ backend/day-03/mini-challenge.py
@@ -0,0 +1,10 @@
+
+
+num= int(input("Enter a number: "))
+
+if num>0:
+    print(f"{num} is a Positive number!")
+elif num<0:
+    print(f"{num} is a Negative number!")
+else:
+    print("That number is a Zero!")
