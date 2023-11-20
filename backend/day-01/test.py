4a58cbe32fd202b24246833158a3af0bab847873
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-20 17:31:29 +0800

    Update test.py

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -2,8 +2,5 @@ name = "Chezter Lei Christian Reblando"
 department= "Department of Software and Web Development"
 about_me = "I love coding"
 
-
-
-
 print(f"Hello, I am {name}. I love {department} and I love in backend even more!")
 print(f"One thing about me is that {about_me}.")
