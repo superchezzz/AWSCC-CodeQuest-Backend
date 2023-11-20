1c3ca8d5cd96f1989c5f004b7ce8d856a1a70261
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-20 17:38:29 +0800

    Update day-2.py

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,10 @@
+#Enterign the three integers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+#Addition of the three integers
+result= num1+num2+num3
+
+#Printing the result
+print("Sum: "+ str(result))
