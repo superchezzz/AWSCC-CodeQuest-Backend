9da4d0ee4b5130f659230357f0f5d1fbfa22f6db
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-21 16:38:18 +0800

    Update mini-project-1.py

--- backend/day-07/mini-project-1.py
+++ backend/day-07/mini-project-1.py
@@ -3,8 +3,7 @@ options = """Options:
 1. Add item to the shopping list
 2. View shopping list
 3. Remove item from the shopping list
-4. Quit
-"""
+4. Quit"""
 
 shopping_list= []
 
@@ -17,14 +16,15 @@ while True:
         print(f"{add_item} has been added to your shopping list.\n")
 
     elif choice_num==2:
-        view_list=shopping_list()
-        print("Your shopping list: \n")
-        print(shopping_list) #still not working
+        print("Your shopping list:")
+        for view_list in shopping_list:
+            print(f"{view_list}\n") #I still don't know how to fix those spaces 
 
     elif choice_num==3:
         remove= (input("Enter the item you want to remove:"))
         shopping_list.remove(remove)
         print(f"{remove} has been removed from your shopping list\n")
+
     elif choice_num==4:
         print("Goodbye!")
         break
