4581309a4d7b7706b6b940b01a0daf173e83a90a
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-21 16:48:22 +0800

    Update mini-project-1.py

--- backend/day-07/mini-project-1.py
+++ backend/day-07/mini-project-1.py
@@ -1,5 +1,5 @@
 
-options = """Options:
+options = """\nOptions:
 1. Add item to the shopping list
 2. View shopping list
 3. Remove item from the shopping list
@@ -11,19 +11,19 @@ while True:
     print(options)
     choice_num= int(input("Enter the number of your choice: "))
     if choice_num==1:
-        add_item= input("Enter the item you want to add: ")
+        add_item= input("Enter the item you want to add: ").capitalize()
         shopping_list.append(add_item)
-        print(f"{add_item} has been added to your shopping list.\n")
+        print(f"{add_item} has been added to your shopping list.")
 
     elif choice_num==2:
         print("Your shopping list: ")
         for view_list in shopping_list:
-            print(f"{view_list}\n") #I still don't know how to fix those spaces 
+            print(f"{view_list}")
 
     elif choice_num==3:
-        remove= (input("Enter the item you want to remove: "))
+        remove= (input("Enter the item you want to remove: ")).capitalize()
         shopping_list.remove(remove)
-        print(f"{remove} has been removed from your shopping list\n")
+        print(f"{remove} has been removed from your shopping list")
 
     elif choice_num==4:
         print("Goodbye!")
