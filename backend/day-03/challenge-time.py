b840951dea9d693698c941dd1d7858c48e817ee8
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-21 17:33:31 +0800

    Update challenge-time.py

--- backend/day-03/challenge-time.py
+++ backend/day-03/challenge-time.py
@@ -3,35 +3,31 @@ print("Welcome to the Adventure Game!")
 
 
 #Asking if the user want to proceed
-print("Would you like to proceed? [(0) Yes: (1) No]")
-proceed= int(input())
+proceed= input("Would you like to proceed? [Yes or No]: ").capitalize()
 
 #Conditional Statements
-if proceed == 0:
-    print("A man is asking for a shelter. Will you let him in? [(0) Yes: (1) No]")
-    man_ask= int(input())
-    if man_ask == 0: 
+if proceed == "Yes":
+    man_ask= input("A man is asking for a shelter. Will you let him in? [Yes or No]: ").capitalize()
+    if man_ask == "Yes": 
         print("Police arrived and asked whether the thief is inside.")
-        print("Will you tell the police about the man?  [(0) Yes: (1) No]")
-        police_ask = input()
-        if police_ask == 0:
+        police_ask = input("Will you tell the police about the man?  [Yes or No]: ")
+        if police_ask == "Yes":
             print("You Win!")
-        elif police_ask==1:
+        elif police_ask == "No":
             print("Game Over")
         else:
             print("Error")
-    elif man_ask==1: 
-        print("He attacked on you. Will you knock him down?  [(0) Yes: (1) No]")
-        attack_you=input()
-        if attack_you == 0:
+    elif man_ask == "No": 
+        attack_you=input("He attacked on you. Will you knock him down?  [Yes or No]: ").capitalize()
+        if attack_you == "Yes":
             print("You Win!")
-        elif attack_you==1:
+        elif attack_you=="No":
             print("Game Over")
         else:
             print("Error")
     else:
         print("Error")
-elif proceed == 1:
+elif proceed == "No":
     print("Thank you!")
 else:
     print("Error")
