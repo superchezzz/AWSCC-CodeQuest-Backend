1d8c70b304c910d97c178363e781c61e91736f69
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-22 19:52:30 +0800

    Create challenge-time.py

---
+++ submissions/day-05/challenge-time.py
@@ -0,0 +1,40 @@
+import random
+
+#Welcome to the game
+print("Welcome to this simple rock-paper-scissor console game!")
+
+#Player Input
+player_1= input("Player 1: ").lower()
+player_2= input("Player 2: ").lower()
+
+#Conditional Statements
+if player_1== "rock":
+    if player_2 == "scissors":
+        print("Player1 Wins!")
+    elif player_2 == "paper":
+        print("Player2 Wins!")
+    elif player_1==player_2:
+        print("Draw!")
+    else:
+        print("Error")
+
+elif player_1 == "paper":
+    if player_2 == "rock":
+        print("Player1 Wins!")
+    elif player_2 == "scissors":
+        print("Player2 Wins!")
+    elif player_1==player_2:
+        print("Draw!")
+
+elif player_1 == "scissors":
+    if player_2=="paper":
+        print("Player1 Wins!")
+    elif player_2=="rock":
+        print("Player2 Wins!")
+    elif player_1==player_2:
+        print("Draw!")
+    else:
+        print("Error")
+
+else:
+    print("Error")
