fabd6b412f9a341ec8e495fdadba31e080101927
Chezter Reblando <144419275+urameshiiii@users.noreply.github.com>
2023-11-27 00:39:12 +0800

    Update day-10-error.py

--- backend/day-10/day-10-error.py
+++ backend/day-10/day-10-error.py
@@ -5,7 +5,14 @@ try:
     a = int(input("Enter a number: "))
     b = int(input("Enter a second number: "))
     quotient = divide(a, b)
+except ValueError as e:
+    print(e)
+    print("Enter only a number pls")
+except ZeroDivisionError as e:
+    print(e)
+    print("You can't divide by zero lol ")
 except Exception as e:
     print(f"Invalid input: {e}")
+
 else:
     print(f"The quotient is {quotient}")
